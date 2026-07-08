0ebe18e7103d26104a157945aa54688b7f0f1ddb
Your Name <mail@gmail.com>
2026-07-08 13:58:14 +0300

    подання join

---
+++ Models/vw_WizardWand.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hogwards.Models
+{
+    public class vw_WizardWand
+    {
+        public string WizardName { get; set; }
+
+        public string House { get; set; }
+
+        public string Material { get; set; }
+    }
+}
